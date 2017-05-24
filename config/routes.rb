bec33173f58faf1f6b2a30cc4dd51cebe093a30b
trinh7293 <trinh7293@gmail.com>
2017-05-24 13:28:54 +0700

    add-rating-and-feedback-1-part1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :products, except: :index
-  devise_for :users
   root "products#index"
   get "/static_pages/*page", to: "static_pages#show"
   get "/manage_product", to: "manage_products#index"
+  devise_for :users
+  resources :products, except: :index
+  resources :feedbacks
+  post "/rate", to: "rater#create", as: "rate"
 end
