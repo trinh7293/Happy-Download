c71c8f367826166656b58d556543bcedbe93a1b1
Chau Giang <tothelegendary@gmail.com>
2017-05-25 14:34:46 +0700

    simple-order1

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,7 @@ class User < ApplicationRecord
     foreign_key: "notifier_id"
   has_many :notifications_as_notifiee, class_name: Notification.name,
     foreign_key: "notifiee_id"
+  has_many :order_details, dependent: :destroy
+  has_many :sales, class_name: OrderDetail.name, foreign_key: "seller_id"
+  has_many :purchases, class_name: OrderDetail.name, foreign_key: "buyer_id"
 end
